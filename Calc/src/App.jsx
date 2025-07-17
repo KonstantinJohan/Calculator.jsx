a041c3a1d014232dbc09a3e6f78d9b1376a39997
Konstantin <konstantin.sjoman@gmail.com>
2025-07-17 20:05:59 +0200

    Button layout and most of css done

--- Calc/src/App.jsx
+++ Calc/src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + TFT + Omar</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+function App() 
+{
+  return(
+  <div className="calc-grid">
+    <div className="output">
+      <div className="firstNumber">1111 + </div>
+        <div className="secNumber">11111</div>
+    </div>
+    <button className="bigDigit">AC</button>
+    <button className="smallDigit">DEL</button>
+    <button className="smallDigit">÷</button>
+    <button className="smallDigit">1</button>
+    <button className="smallDigit">2</button>
+    <button className="smallDigit">3</button>
+    <button className="smallDigit">*</button>
+    <button className="smallDigit">4</button>
+    <button className="smallDigit">5</button>
+    <button className="smallDigit">6</button>
+    <button className="smallDigit">+</button>
+    <button className="smallDigit">7</button>
+    <button className="smallDigit">8</button>
+    <button className="smallDigit">9</button>
+    <button className="smallDigit">-</button>
+    <button className="smallDigit">.</button>
+    <button className="smallDigit">0</button>
+    <button className="bigDigit">=</button>
+  </div>
   )
 }
 
