c49cfe3b8cdb317080c63df1568515c178bb64f9
Konstantin <konstantin.sjoman@gmail.com>
2025-07-23 12:39:19 +0200

    Removed unused import

--- Calc/src/App.jsx
+++ Calc/src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from 'react'
+import { useReducer} from 'react'
 import NumberButtons from './NumberButtons'
 import FunctionButtons from './FunctionButtons'
 import './App.css'
